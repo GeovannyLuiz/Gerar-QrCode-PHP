a73542137cab6768e3bc957dc4e9a2b7757424b0
Geovanny Luiz <87313662+GeovannyLuiz@users.noreply.github.com>
2023-02-10 14:51:50 -0300

    Add files via upload

--- geradorQrCode.php
+++ geradorQrCode.php
@@ -3,6 +3,14 @@ error_reporting(0);
 if($_POST['texto'] != ""){
 $chars = array("(",")",",","-","","/",".",":");
 $data = str_replace($chars,'',$_POST['texto']);
+	if(isset($_POST['mod11'])){
+		if(is_numeric($data)){
+			$data = $data.modulo_11($data);
+		}else{
+			echo json_encode(array('status' => false , 'msg' => 'Não foi possível gerar o QrCode pois os dados digitados não são numéricos, tente novamente ou desative o Módulo 11'));
+			exit();
+		}
+	}
 	if(file_exists(__DIR__.'/qrcodegerados/QrCode_'.$data.'.png')){
 		header('Content-Type: application/json');
 		echo json_encode(array('status' => false , 'msg' => 'QrCode já Gerado!!! Para gerar um NOVO, atualize sua página apertando (F5)'));
@@ -48,5 +56,61 @@ echo json_encode(array('status' => true , 'cod' => $data));
 }else{
 	header('Content-Type: application/json');
 	echo json_encode(array('status' => false , 'msg' => 'Dados inválidos tente novamente....'));
+}
+
+
+function modulo_11($num, $base=9, $r=0)  {
+    /**
+     *   Autor:
+     *           Pablo Costa <pablo@users.sourceforge.net>
+     *
+     *   Função:
+     *    Calculo do Modulo 11 para geracao do digito verificador 
+     *    de boletos bancarios conforme documentos obtidos 
+     *    da Febraban - www.febraban.org.br 
+     *
+     *   Entrada:
+     *     $num: string numérica para a qual se deseja calcularo digito verificador;
+     *     $base: valor maximo de multiplicacao [2-$base]
+     *     $r: quando especificado um devolve somente o resto
+     *
+     *   Saída:
+     *     Retorna o Digito verificador.
+     *
+     *   Observações:
+     *     - Script desenvolvido sem nenhum reaproveitamento de código pré existente.
+     *     - Assume-se que a verificação do formato das variáveis de entrada é feita antes da execução deste script.
+     */                                        
+
+    $soma = 0;
+    $fator = 2;
+
+    /* Separacao dos numeros */
+    for ($i = strlen($num); $i > 0; $i--) {
+        // pega cada numero isoladamente
+        $numeros[$i] = substr($num,$i-1,1);
+        // Efetua multiplicacao do numero pelo falor
+        $parcial[$i] = $numeros[$i] * $fator;
+        // Soma dos digitos
+        $soma += $parcial[$i];
+        if ($fator == $base) {
+            // restaura fator de multiplicacao para 2 
+            $fator = 1;
+        }
+        $fator++;
+    }
+
+    /* Calculo do modulo 11 */
+    if ($r == 0) {
+        $soma *= 10;
+        $digito = $soma % 11;
+        if ($digito == 10) {
+            $digito = 0;
+        }
+        return $digito;
+    } elseif ($r == 1){
+        $resto = $soma % 11;
+        return $resto;
+    }
 }
 ?>
